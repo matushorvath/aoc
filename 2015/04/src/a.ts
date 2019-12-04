b85ead2b6430176c61d687a9b3ec2b2b901bc088
Matus Horvath <matus.horvath@gmail.com>
2019-12-04 04:45:22 +0100

    Add 2015/04

---
+++ 2015/04/src/a.ts
@@ -0,0 +1,20 @@
+import crypto from 'crypto';
+
+const main = async () => {
+    const input = 'ckczppom';
+
+    let value = 1;
+    while (true) {
+        const hash = crypto.createHash('md5');
+        hash.update(input + value);
+        if (hash.digest('hex').startsWith('00000')) {
+            console.log(value);
+            return;
+        }
+        value = value + 1;
+    }
+};
+
+main()
+    .then(() => console.log('done'))
+    .catch(error => console.log('error:', error));

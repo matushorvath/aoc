83edc8b8533f1a2483c43ae6e6d76cc5709db55f
Matus Horvath <matus.horvath@gmail.com>
2019-12-05 09:42:39 +0100

    Improve 05b

--- 2019/05/src/b.ts
+++ 2019/05/src/b.ts
@@ -3,17 +3,34 @@ import { promises as fs } from 'fs';
 interface State {
     ip: number;
     mem: number[];
-    indata: number[];
-    outdata: number[];
+    ins: number[];
+    outs: number[];
 };
 
-const param = (s: State, i: number, ops: number[], ms: number[]) => {
-    if (ms[i] === 0) { // position mode
-        return s.mem[ops[i]];
-    } else if (ms[i] === 1) { // immediate mode
-        return ops[i];
-    } else {
-        throw new Error('mode error');
+interface Op {
+    oc: number;
+    ops: number[];
+    mds: number[];
+};
+
+const getOp = (s: State) => {
+    const [ocm, ...ops] = s.mem.slice(s.ip, s.ip + 4)
+    const oc = ocm % 100;
+    const mds = [100, 1000, 10000].map(x => Math.trunc(ocm / x) % 10);
+
+    console.log('op', { ip: s.ip, oc, ops, mds });
+
+    return { oc, ops, mds };
+};
+
+const getParam = (s: State, o: Op, i: number) => {
+    switch (o.mds[i]) {
+        case 0: // position mode
+            return s.mem[o.ops[i]];
+        case 1: // immediate mode
+            return o.ops[i];
+        default:
+            throw new Error('mode error');
     }
 };
 
@@ -24,60 +41,56 @@ const main = async () => {
     const s: State = {
         ip: 0,
         mem: input.split(',').map(s => Number(s)),
-        indata: [5],
-        outdata: []
+        ins: [1],
+        outs: []
     };
 
     while (true) {
-        const [ocm, ...ops] = s.mem.slice(s.ip, s.ip + 4)
-        const oc = ocm % 100;
-        const ms = [100, 1000, 10000].map(x => Math.trunc(ocm / x) % 10);
+        const o = getOp(s);
 
-        console.log('op', { ip: s.ip, oc, ops, ms });
-
-        switch (oc) {
+        switch (o.oc) {
             case 1: // add
-                s.mem[ops[2]] = param(s, 0, ops, ms) + param(s, 1, ops, ms);
+                s.mem[o.ops[2]] = getParam(s, o, 0) + getParam(s, o, 1);
                 s.ip += 4;
                 break;
             case 2: // mul
-                s.mem[ops[2]] = param(s, 0, ops, ms) * param(s, 1, ops, ms);
+                s.mem[o.ops[2]] = getParam(s, o, 0) * getParam(s, o, 1);
                 s.ip += 4;
                 break;
             case 3: // in
-                s.mem[ops[0]] = s.indata.shift();
+                s.mem[o.ops[0]] = s.ins.shift();
                 s.ip += 2;
                 break;
             case 4: // out
-                const v = param(s, 0, ops, ms);
+                const v = getParam(s, o, 0);
                 console.log('out', v);
-                s.outdata.push(v);
+                s.outs.push(v);
                 s.ip += 2;
                 break;
             case 5: // jnz
-                if (param(s, 0, ops, ms) !== 0) {
-                    s.ip = param(s, 1, ops, ms)
+                if (getParam(s, o, 0) !== 0) {
+                    s.ip = getParam(s, o, 1)
                 } else {
                     s.ip += 3;
                 }
                 break;
             case 6: // jz
-                if (param(s, 0, ops, ms) === 0) {
-                    s.ip = param(s, 1, ops, ms)
+                if (getParam(s, o, 0) === 0) {
+                    s.ip = getParam(s, o, 1)
                 } else {
                     s.ip += 3;
                 }
                 break;
             case 7: // lt
-                s.mem[ops[2]] = param(s, 0, ops, ms) < param(s, 1, ops, ms) ? 1 : 0;
+                s.mem[o.ops[2]] = getParam(s, o, 0) < getParam(s, o, 1) ? 1 : 0;
                 s.ip += 4;
                 break;
             case 8: // eq
-                s.mem[ops[2]] = param(s, 0, ops, ms) === param(s, 1, ops, ms) ? 1 : 0;
+                s.mem[o.ops[2]] = getParam(s, o, 0) === getParam(s, o, 1) ? 1 : 0;
                 s.ip += 4;
                 break;
             case 99: // hlt
-                console.log('all outs', s.outdata);
+                console.log('all outs', s.outs);
                 return;
             default:
                 throw new Error('opcode error');

e7500e476a1e38a7a8cd38a2478c2dc365b04be4
Matus Horvath <matus.horvath@sap.com>
2019-12-11 19:34:54 +0100

    2019 11 ab

---
+++ 2019/11/src/b.ts
@@ -0,0 +1,52 @@
+import { promises as fs } from 'fs';
+import { Vm } from './vm-b';
+
+const field: bigint[][] = [];
+
+const getField = (x: number, y: number) => {
+    if (field[x] === undefined) return BigInt(0);
+    else return field[x][y] === BigInt(1) ? BigInt(1) : BigInt(0);
+};
+
+const setField = (x: number, y: number, v: bigint) => {
+    if (field[x] === undefined) field[x] = [];
+    field[x][y] = v;
+};
+
+const main = async () => {
+    const input = await fs.readFile('input', 'utf8');
+    const mem = input.split(',').reduce((m, s, i) => ({ [`${i}`]: BigInt(s), ...m }), {});
+
+    const vm = new Vm(0, mem);
+
+    let x = 0, y = 0;
+    let dx = 0, dy = -1;
+
+    setField(0, 0, BigInt(1));
+
+    while (!vm.halted) {
+        console.log('robot pos', [x, y], [dx, dy]);
+
+        const inn = [getField(x, y)];
+        console.log('robot in', inn);
+
+        const [color, turn] = vm.run(inn);
+        console.log('robot out', [color, turn]);
+        setField(x, y, color);
+
+        if (turn === BigInt(0)) {
+            // left
+            [dx, dy] = [dy, -dx];
+        } else {
+            // right
+            [dx, dy] = [-dy, dx];
+        }
+        [x, y] = [x + dx, y + dy];
+    }
+
+    console.log(field.reverse().map(r => r.map(c => c === BigInt(1) ? 'X' : ' ').join('')).join('\n'));
+};
+
+main()
+    .then(() => console.log('done'))
+    .catch(error => console.log('error:', error));

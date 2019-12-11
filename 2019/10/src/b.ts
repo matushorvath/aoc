09cb2884930c77a44fe4177ebe22c12222638e91
Matus Horvath <matus.horvath@gmail.com>
2019-12-11 05:56:52 +0100

    2019 10b

--- 2019/10/src/b.ts
+++ 2019/10/src/b.ts
@@ -61,18 +61,33 @@ const samples = [`
 .#.#.###########.###
 #.#.#.#####.####.###
 ###.##.####.##.#..##
+`, `
+.#....#####...#..
+##...##.#####..##
+##...#...#.#####.
+..#.....X...###..
+..#.#.....#....##
 `
 ];
 
+type Vec = { x: number, y: number };
+
 const main = async () => {
     const input = await fs.readFile('input', 'utf8');
+    //const input = samples[4];
     const field = input.trim().split(/\r?\n/);
     const rocks = field.reduce((fr, r, y) => [...fr, ...r.split('').reduce((fp, p, x) => p === '#' ? [...fp, { x, y }] : fp, [])], [])
 
     let max = 0;
+    let lrock: Vec;
+    let ldirs: { [key: string]: { d: Vec, r: Vec[] } };
 
+    // const rock1 = { x: 8, y: 3 }; // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    // {
     for (const rock1 of rocks) {
-        const dirs: { x: number, y: number }[] = [];
+        //console.log('\n', rock1);
+
+        const dirs: { [key: string]: { d: Vec, r: Vec[] } } = {};
 
         for (const rock2 of rocks) {
             if (rock1.x !== rock2.x || rock1.y !== rock2.y) {
@@ -81,15 +96,72 @@ const main = async () => {
                 const gcd = mathjs.gcd(x, y);
 
                 const dir = { x: x / gcd, y: y / gcd };
-                dirs[dir.x * 100 + dir.y] = dir;
+                const key = JSON.stringify(dir);
+
+                if (key in dirs) {
+                    dirs[key].r.push(rock2);
+                } else {
+                    dirs[key] = { d: dir, r: [rock2] };
+                }
             }
         }
 
         const len = Object.keys(dirs).length;
-        max = Math.max(max, len);
+        //console.log(len);
+
+        if (len > max) {
+            max = len;
+            lrock = rock1;
+            ldirs = dirs;
+        }
     }
 
-    console.log(max);
+    console.log(max, lrock);
+
+    // sort rocks in each direction by distance
+    for (const dir of Object.values(ldirs)) {
+        dir.r.sort((rock1, rock2) =>
+            Math.abs(rock1.x - lrock.x) - Math.abs(rock2.x - lrock.x) + 
+            Math.abs(rock1.y - lrock.y) - Math.abs(rock2.y - lrock.y)
+        ).reverse();
+    }
+
+    // sort directions by angle
+    const lcm = Object.values(ldirs).reduce((p, c) => mathjs.lcm(p, c.d.x || 1), 1);
+
+    const order = Object.keys(ldirs);
+    console.log(order);
+
+    order.sort((o1, o2) => Math.atan2(-ldirs[o1].d.x, ldirs[o1].d.y) - Math.atan2(-ldirs[o2].d.x, ldirs[o2].d.y));
+    /*{
+        if ((ldirs[o1].d.x > 0) !== (ldirs[o2].d.x > 0)) {
+            const val = ldirs[o2].d.x - ldirs[o1].d.x;
+            console.log(ldirs[o1], ldirs[o2], 'mix', val);
+            return val;
+        } else {
+            const val = ldirs[o1].d.y * lcm / ldirs[o1].d.x - ldirs[o2].d.y * lcm / ldirs[o2].d.x;
+            console.log(ldirs[o1], ldirs[o2], 'sam', val);
+            return val;
+        }
+    });*/
+
+    console.log('\n');
+    order.map(o => console.log(ldirs[o]));
+
+    console.log('\n');
+
+    let index = 0;
+    let changed = true;
+    while (changed) {
+        changed = false;
+        for (const dir of order.map(o => ldirs[o])) {
+            if (dir.r.length > 0) {
+                changed = true;
+                index += 1;
+                console.log(index, dir.r.pop());
+            }
+        }
+    }
 };
 
 main()

0c72b3e2ab05e258e510b15209fe3ee565d99472
Matus Horvath <matus.horvath@gmail.com>
2019-12-11 04:48:58 +0100

    2019 10 a

--- 2019/10/src/a.ts
+++ 2019/10/src/a.ts
@@ -1,109 +1,28 @@
 import { promises as fs } from 'fs';
 import * as mathjs from 'mathjs';
 
-type Dir = { x: number, y: number };
-
-const equalDir = (d1: Dir, d2: Dir) => {
-    return d1.x === d2.x && d1.y === d2.y;
-};
-
-const samples = [`
-.#..#
-.....
-#####
-....#
-...##
-`, `
-......#.#.
-#..#.#....
-..#######.
-.#.#.###..
-.#..#.....
-..#....#.#
-#..#....#.
-.##.#..###
-##...#..#.
-.#....####
-`, `
-#.#...#.#.
-.###....#.
-.#....#...
-##.#.#.#.#
-....#.#.#.
-.##..###.#
-..#...##..
-..##....##
-......#...
-.####.###.
-`, `
-.#..#..###
-####.###.#
-....###.#.
-..###.##.#
-##.##.#.#.
-....###..#
-..#.#..#.#
-#..#.#.###
-.##...##.#
-.....#.#..
-`, `
-.#..##.###...#######
-##.############..##.
-.#.######.########.#
-.###.#######.####.#.
-#####.##.#.##.###.##
-..#####..#.#########
-####################
-#.####....###.#.#.##
-##.#################
-#####.##.###..####..
-..######..##.#######
-####.##.####...##..#
-.#####..#.######.###
-##...#.##########...
-#.##########.#######
-.####.#.###.###.#.##
-....##.##.###..#####
-.#.#.###########.###
-#.#.#.#####.####.###
-###.##.####.##.#..##
-`
-];
-
 const main = async () => {
     const input = await fs.readFile('input', 'utf8');
-    //const input = samples[4];
     const field = input.trim().split(/\r?\n/);
     const rocks = field.reduce((fr, r, y) => [...fr, ...r.split('').reduce((fp, p, x) => p === '#' ? [...fp, { x, y }] : fp, [])], [])
-
-    //console.log(rocks);
 
     let max = 0;
 
     for (const rock1 of rocks) {
-        const dirs: { [key: string]: Dir & { r: Dir[] } } = {};
+        const dirs: { [key: string]: { x: number, y: number } } = {};
+
         for (const rock2 of rocks) {
-            if (!equalDir(rock1, rock2)) {
+            if (rock1.x !== rock2.x || rock1.y !== rock2.y) {
                 let x = rock2.x - rock1.x;
                 let y = rock2.y - rock1.y;
                 const gcd = mathjs.gcd(x, y);
 
                 const dir = { x: x / gcd, y: y / gcd };
-
-                const key = JSON.stringify(dir);
-                if (key in dirs) {
-                    dirs[key].r.push(rock2);
-                } else {
-                    dirs[key] = { ...dir, r: [rock2] };
-                }
-
-                //console.log(rock1, rock2, dir);
+                dirs[JSON.stringify(dir)] = dir;
             }
         }
 
         const len = Object.keys(dirs).length;
-        //console.log(rock1, len);
-        //Object.values(dirs).map(d => console.log(d));
         max = Math.max(max, len);
     }
 
--- 2019/10/src/b.ts
+++ 2019/10/src/b.ts
@@ -1,21 +1,114 @@
 import { promises as fs } from 'fs';
-import { Vm } from './vm-b';
+import * as mathjs from 'mathjs';
+
+const samples = [`
+.#..#
+.....
+#####
+....#
+...##
+`, `
+......#.#.
+#..#.#....
+..#######.
+.#.#.###..
+.#..#.....
+..#....#.#
+#..#....#.
+.##.#..###
+##...#..#.
+.#....####
+`, `
+#.#...#.#.
+.###....#.
+.#....#...
+##.#.#.#.#
+....#.#.#.
+.##..###.#
+..#...##..
+..##....##
+......#...
+.####.###.
+`, `
+.#..#..###
+####.###.#
+....###.#.
+..###.##.#
+##.##.#.#.
+....###..#
+..#.#..#.#
+#..#.#.###
+.##...##.#
+.....#.#..
+`, `
+.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##
+`
+];
+
+
+type Dir = { x: number, y: number };
+
+const equalDir = (d1: Dir, d2: Dir) => {
+    return d1.x === d2.x && d1.y === d2.y;
+};
 
 const main = async () => {
     const input = await fs.readFile('input', 'utf8');
-    //const input = await fs.readFile('../intcode/input', 'utf8');
-    //const input = '109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99';
-    //const input = '1102,34915192,34915192,7,4,7,99,0';
-    //const input = '104,1125899906842624,99';
-    const mem = input.split(',').reduce((m, s, i) => ({ [`${i}`]: BigInt(s), ...m }), {});
+    //const input = samples[4];
+    const field = input.trim().split(/\r?\n/);
+    const rocks = field.reduce((fr, r, y) => [...fr, ...r.split('').reduce((fp, p, x) => p === '#' ? [...fp, { x, y }] : fp, [])], [])
 
-    //console.log(mem);
+    //console.log(rocks);
 
-    for (let i = 0; i < 10; i += 1) {
-        const vm = new Vm(0, mem);
-        console.log(vm.run([BigInt(2)]));
-        //console.log(vm.dasm());
+    let max = 0;
+
+    for (const rock1 of rocks) {
+        const dirs: { [key: string]: Dir & { r: Dir[] } } = {};
+        for (const rock2 of rocks) {
+            if (!equalDir(rock1, rock2)) {
+                let x = rock2.x - rock1.x;
+                let y = rock2.y - rock1.y;
+                const gcd = mathjs.gcd(x, y);
+
+                const dir = { x: x / gcd, y: y / gcd };
+
+                const key = JSON.stringify(dir);
+                if (key in dirs) {
+                    dirs[key].r.push(rock2);
+                } else {
+                    dirs[key] = { ...dir, r: [rock2] };
+                }
+
+                //console.log(rock1, rock2, dir);
+            }
+        }
+
+        const len = Object.keys(dirs).length;
+        //console.log(rock1, len);
+        //Object.values(dirs).map(d => console.log(d));
+        max = Math.max(max, len);
     }
+
+    console.log(max);
 };
 
 main()

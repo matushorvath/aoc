f5f3558a96047541b3c0a24cb854ce13400df681
Matus Horvath <matus.horvath@sap.com>
2019-12-19 01:41:36 +0100

    2019 16 b

--- 2019/16/src/b.ts
+++ 2019/16/src/b.ts
@@ -1,47 +1,27 @@
 import { promises as fs } from 'fs';
 
 const main = async () => {
-    //const input = await fs.readFile('input', 'utf8');
-    //const input = '03036732577212944063491565474664';
-    const input = '2357235723572357235723572357235723572357';
-    let vec = input.trimRight().split('').map(n => Number(n));
+    const input = await fs.readFile('input', 'utf8');
+//    const input = '03036732577212944063491565474664';
+//    const input = '02935109699940807407585447034323';
+//      const input = '03081770884921959731165446850517';
+    const data = input.trimRight().split('').map(n => Number(n));
 
-    const start = 0;//Number(input.substr(0, 8));
-    const pat = [0, 1, 0, -1];
+    const start = Number(input.substr(0, 7));
+    const offset = start % data.length;
+    const repeats = 10000 - Math.ceil((start + 1) / data.length);
 
-/*
-lcm:
-32,4*1 = 32 = 1
-32,4*2 = 32 = 1
-32,4*3 = 96 = 3
-32,4*4 = 32 = 1
-32,4*5 = 160 = 5
-32,4*6 = 96 = 3
-32,4*7 = 224 = 7
-32,4*8 = 32 = 1
-32,4*9 = 288  = 9
+    console.log(start, offset, repeats, data.length)
 
-10000 / 1 = 10000 (times one period) + 10000 % 1 (1 extra copy)
+    const vec = data.slice(offset).concat(...Array(repeats).fill(data));
+    console.log(vec.length);
 
----
-
-32,4*32 = 128 = 4
-*/
-
-    for (let k = 0; k < 10; k += 1) {
-        const oj = [];
-        for (let j = 0; j < vec.length; j += 1) {
-            const oi = [];
-            for (let i = 0; i < vec.length; i += 1) {
-                oi[i] = vec[i] * pat[Math.trunc((i + 1) % (4 * (j + 1)) / (j + 1))];
-            }
-            console.log('oi', k, j, oi);
-            oj[j] = Math.abs(oi.reduce((p, c) => p + c, 0) % 10);
+    for (let i = 0; i < 100; i += 1) {
+        for (let j = vec.length - 1; j >= 0; j -= 1) {
+            vec[j] = (vec[j] + (vec[j + 1] || 0)) % 10;
         }
-        console.log('oj', k, oj.join(''));
-        vec = oj;
     }
-    console.log(vec.join(''));//.substr(start, 8));
+    console.log(vec.slice(0, 8).join(''));
 };
 
 main()

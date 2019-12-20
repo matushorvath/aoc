2c8a0ff2ff4fdb7083849e3f6c7c0046a073e7d0
Matus Horvath <matus.horvath@sap.com>
2019-12-20 22:16:58 +0100

    2019 20 a wip

--- 2019/20/src/a.ts
+++ 2019/20/src/a.ts
@@ -4,28 +4,22 @@ const main = async () => {
     const input = await fs.readFile('input-a1', 'utf8');
     let data = input.slice(0, -1).split(/\r?\n/).map(l => l.split(''));
 
-    data[1] = data[0].map((_, i) => data[0][i] + data[1][i]);
+    const mx = Math.trunc(data.length / 2);
+    const my = Math.trunc(data[0].length / 2);
 
-    const s = data.length - 1;
-    data[s - 1] = data[s].map((_, i) => data[s - 1][i] + data[s][i]);
+    const [x0, y0] = [1, 1];
+    const [x1, y1] = [
+        data.slice(x0 + 1).findIndex(l => l[my].match(/^[A-Z ]$/)) + x0 + 1,
+        data[mx].slice(y0 + 1).findIndex(p => p.match(/^[A-Z ]$/)) + y0 + 1
+    ];
+    const [x2, y2] = [
+        data.slice(x1).findIndex(l => !l[my].match(/^[A-Z ]$/)) + x1 - 1,
+        data[mx].slice(y1).findIndex(p => !p.match(/^[A-Z ]$/)) + y1 - 1
+    ];
+    const [x3, y3] = [data.length - 2, data[0].length - 2];
 
-    data = data.slice(1, -1);
-    data = data.map(l => [l.slice(0, 2).join(''), ...l.slice(2, -2), l.slice(-2).join('')]);
-
-    const f = data[Math.trunc(data.length / 2)].findIndex(p => p === ' ');
-    data = data.map(l => l
-    const e = data[0].length - [...data[Math.trunc(data.length / 2)]].reverse().findIndex(p => p === ' ') - 1;
-
-    const t = data.findIndex(l => l.some(p => p === ' '));
-    data[t] = data[t].map((p, i) => p.match(/^[A-Z]$/) ? data[t][i] + data[t + 1][i] : p);
-    data[t + 1] = data[t + 1].map((p, i) => p.match(/^[A-Z]$/) ? '' : p);
-
-    const b = data.length - [...data].reverse().findIndex(l => l.some(p => p === ' ')) - 1;
-    data[b] = data[b].map((p, i) => p.match(/^[A-Z]$/) ? data[b - 1][i] + data[b][i] : p);
-    data[b - 1] = data[b - 1].map((p, i) => p.match(/^[A-Z]$/) ? '' : p);
-
-    data = data.map(l => l.map(p => p.trim()));
-
+    console.log(x0, x1, x2, x3);
+    console.log(y0, y1, y2, y3);
     console.log(data.map(l => l.map(p => ('  ' + p).slice(-2)).join(',')).join('\n'));
 };
 

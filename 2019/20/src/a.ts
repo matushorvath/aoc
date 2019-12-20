44d936bcc96cc2837d9e6c9991c0155c134ca1c0
Matus Horvath <matus.horvath@sap.com>
2019-12-20 22:26:54 +0100

    2019 20 a loading

--- 2019/20/src/a.ts
+++ 2019/20/src/a.ts
@@ -18,8 +18,31 @@ const main = async () => {
     ];
     const [x3, y3] = [data.length - 2, data[0].length - 2];
 
-    console.log(x0, x1, x2, x3);
-    console.log(y0, y1, y2, y3);
+    for (let x = x0 + 1; x < x3; x += 1) {
+        data[x][y0] = data[x][y0 - 1] + data[x][y0];
+        data[x][y0 - 1] = ' ';
+        data[x][y3] = data[x][y3] + data[x][y3 + 1];
+        data[x][y3 + 1] = ' ';
+    }
+    for (let x = x1 + 1; x < x2; x += 1) {
+        data[x][y1] = data[x][y1] + data[x][y1 + 1];
+        data[x][y1 + 1] = ' ';
+        data[x][y2] = data[x][y2 - 1] + data[x][y2];
+        data[x][y2 - 1] = ' ';
+    }
+    for (let y = y0 + 1; y < y3; y += 1) {
+        data[x0][y] = data[x0 - 1][y] + data[x0][y];
+        data[x0 - 1][y] = ' ';
+        data[x3][y] = data[x3][y] + data[x3 + 1][y];
+        data[x3 + 1][y] = ' ';
+    }
+    for (let y = y1 + 1; y < y2; y += 1) {
+        data[x1][y] = data[x1][y] + data[x1 + 1][y];
+        data[x1 + 1][y] = ' ';
+        data[x2][y] = data[x2 - 1][y] + data[x2][y];
+        data[x2 - 1][y] = ' ';
+    }
+
     console.log(data.map(l => l.map(p => ('  ' + p).slice(-2)).join(',')).join('\n'));
 };
 

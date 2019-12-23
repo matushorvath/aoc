e746bf73c915d71095a31b37164ac7b3f8669a3f
Matus Horvath <matus.horvath@gmail.com>
2019-12-23 17:58:57 +0100

    2019 22 b imp

--- 2019/23/src/b.ts
+++ 2019/23/src/b.ts
@@ -1,35 +1,39 @@
-import { promises as fs, exists } from 'fs';
+import { promises as fs } from 'fs';
 import { Vm } from './vm-b';
 
 type Queue = [bigint, bigint][];
-type Item = { vm: Vm, q: Queue, idle: number };
+type Item = { vm: Vm, q: Queue, idle: boolean };
+
 const data: Item[] = [];
 
-let log = false;
+const natys = new Set<number>();
+let nat: [bigint, bigint];
 
 async function* getIns(d: Item) {
     yield BigInt(d.vm.id);
 
     while (true) {
         if (d.q.length === 0) {
-            //if (log) console.log('in', d.vm.id, -1);
-            if (nat && data.every(d => d.idle > 0 && d.q.length === 0)) {
-                console.log('---------------> idle', d.vm.id, nat);
+            //console.log('in', d.vm.id, -1);
+            if (nat && data.every(d => d.idle && d.q.length === 0)) {
+                //console.log('---------------> idle', d.vm.id, nat);
                 //console.log('ips', data.map(d => d.vm.ip));
                 data[0].q.push(nat);
+
                 if (natys.has(Number(nat[1]))) {
-                    console.log('===============================', nat[1]);
+                    console.log('dup nat y', nat[1]);
                     process.exit(0);
                 }
                 natys.add(Number(nat[1]));
             }
 
-            d.idle += 1;
+            d.idle = true;
             yield BigInt(-1);
         } else {
             const [x, y] = d.q.shift();
-            console.log('in', d.vm.id, [x, y]);
-            d.idle = 0;
+            d.idle = false;
+
+            //console.log('in', d.vm.id, [x, y]);
 
             yield x;
             yield y;
@@ -37,24 +41,21 @@ async function* getIns(d: Item) {
     }
 }
 
-const natys = new Set<number>();
-let nat: [bigint, bigint];
-
-const runOne = async (d: Item) => {
+const runVm = async (d: Item) => {
     let outs: bigint[] = [];
 
     for await (const out of d.vm.run(getIns(d))) {
         outs.push(out);
 
         if (outs.length === 3) {
-            console.log('out', d.vm.id, outs);
+            //console.log('out', d.vm.id, outs);
 
             const [abi, x, y] = outs;
             outs = [];
 
             const a = Number(abi);
             if (a === 255) {
-                console.log('---------------> nat', d.vm.id, [x, y]);
+                //console.log('---------------> nat', d.vm.id, [x, y]);
                 nat = [x, y];
             } else {
                 data[a].q.push([x, y]);
@@ -72,11 +73,11 @@ const main = async () => {
         data.push({
             vm: new Vm(i, { ...mem }),
             q: [],
-            idle: 0
+            idle: false
         });
     }
 
-    await Promise.all(data.map(d => runOne(d)));
+    await Promise.all(data.map(d => runVm(d)));
 };
 
 main()

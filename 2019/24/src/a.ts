dc5612d662282d1764088fd0a5a1fe9f75c77820
Matus Horvath <matus.horvath@gmail.com>
2019-12-24 11:00:48 +0100

    2019 24 a bad

--- 2019/24/src/a.ts
+++ 2019/24/src/a.ts
@@ -12,14 +12,29 @@ const dump = (d: number) => {
     console.log(out.map(r => r.join('')).join('\n'));
 };
 
+const p = 0b10001010001;
+
+const nbrs = (d: number, i: number) => {
+    const ps = i < 5 ? (p >> 5 - i) : (p << i - 5);
+    const rs = (d & ps);
+    const r = i < 5 ? (rs << 5 - i) : (rs >> i - 5);
+    //console.log('p', i, ps.toString(2));
+    console.log('n', i, r.toString(2));
+};
+
 const main = async () => {
     const input = await fs.readFile('input', 'utf8');
 
     const data = input.replace(/\r?\n/g, '').split('').reverse()
-        .reduce((t, c) => (t << 1) + (c === '#' ? 1 : 0), 0);
+         .reduce((t, c) => (t << 1) + (c === '#' ? 1 : 0), 0);
 
     console.log(data);
     dump(data);
+
+    console.log('d', data.toString(2));
+    for (let i = 0; i < 25; i += 1) {
+        nbrs(data, i);
+    }
 };
 
 main()

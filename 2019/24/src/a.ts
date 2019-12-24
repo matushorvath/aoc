5dbcebd7d05e50b305bc45446c2e5788be67cbb1
Matus Horvath <matus.horvath@gmail.com>
2019-12-24 11:11:59 +0100

    2019 24 a wip

--- 2019/24/src/a.ts
+++ 2019/24/src/a.ts
@@ -1,39 +1,24 @@
 import { promises as fs } from 'fs';
 
-const dump = (d: number) => {
-    const out: string[][] = [];
-    for (let r = 0; r < 5; r += 1) {
-        out[r] = [];
-        for (let c = 0; c < 5; c += 1) {
-            out[r].push((d & 1) ? '#' : '.');
-            d = d >> 1;
-        }
-    }
-    console.log(out.map(r => r.join('')).join('\n'));
+const dump = (d: number[][]) => {
+    console.log(d.map(r => r.map(c => c === 1 ? '#' : '.').join('')).join('\n'));
 };
 
-const p = 0b10001010001;
-
-const nbrs = (d: number, i: number) => {
-    const ps = i < 5 ? (p >> 5 - i) : (p << i - 5);
-    const rs = (d & ps);
-    const r = i < 5 ? (rs << 5 - i) : (rs >> i - 5);
-    //console.log('p', i, ps.toString(2));
-    console.log('n', i, r.toString(2));
+const nbrs = (d: number[][], r: number, c: number) => {
+    return (d[r - 1]?.[c] ?? 0) + (d[r + 1]?.[c] ?? 0) + (d[r][c - 1] ?? 0) + (d[r][c + 1] ?? 0);
 };
 
 const main = async () => {
     const input = await fs.readFile('input', 'utf8');
 
-    const data = input.replace(/\r?\n/g, '').split('').reverse()
-         .reduce((t, c) => (t << 1) + (c === '#' ? 1 : 0), 0);
+    const d = input.trimRight().split(/\r?\n/).map(r => r.split('').map(c => c === '#' ? 1 : 0));
 
-    console.log(data);
-    dump(data);
+    dump(d);
 
-    console.log('d', data.toString(2));
-    for (let i = 0; i < 25; i += 1) {
-        nbrs(data, i);
+    for (let r = 0; r < 5; r += 1) {
+        for (let c = 0; c < 5; c += 1) {
+            console.log('n', r, c, nbrs(d, r, c));
+        }
     }
 };
 

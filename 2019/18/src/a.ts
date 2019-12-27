d03a1aa980a9382237ca09720904258b50109a42
Matus Horvath <matus.horvath@sap.com>
2019-12-27 01:49:57 +0100

    2019 20 a wip

--- 2019/18/src/a.ts
+++ 2019/18/src/a.ts
@@ -36,17 +36,35 @@ const logField = (field: string[][], x: number, y: number) => {
 const main = async () => {
 //    const input = await fs.readFile('input', 'utf8');
 
-    const input = 
-`#########
-#b.A.@.a#
-#########
+//     const input = 
+// `#########
+// #b.A.@.a#
+// #########
+// `;
+//     const input =
+// `########################
+// #f.D.E.e.C.b.A.@.a.B.c.#
+// ######################.#
+// #d.....................#
+// ########################
+// `;
+const input =
+`########################
+#...............b.C.D.f#
+#.######################
+#.....@.a.B.c.d.A.e.F.g#
+########################
 `;
 
     const field = input.trimRight().split(/\r?\n/).map(r => r.split(''));
 
     let [x, y] = field.reduce((tr, r, ri) => tr[1] >= 0 ? tr : [ri, r.indexOf('@')], [-1, -1]);
     aset(field, x, y, '.');
-    logField(field, x, y);
+    //logField(field, x, y);
+
+    const mxk = field.reduce((tr, r) => [...tr, ...r.filter(c => c >= 'a' && c <= 'z')], []).sort().pop();
+    const kl = mxk.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
+    //console.log(mxk, kl);
 
     const vals: { [key: string]: number }[][] = [];
     const vist: { [key: string]: boolean }[][] = [];
@@ -54,7 +72,7 @@ const main = async () => {
 
     let keys = '';
 
-    while (keys.length < 2) {
+    while (keys.length < kl) {
         for (const [i, j] of [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]]) {
             const isKey = field[i][j] >= 'a' && field[i][j] <= 'z';
             const isDoor = field[i][j] >= 'A' && field[i][j] <= 'Z';
@@ -68,9 +86,9 @@ const main = async () => {
 
         vset(vist, x, y, keys, true);
 
-        console.log(vist)
-        console.log('xyk', [x, y, keys]);
-        logField(field, x, y);
+        //console.log(vist)
+        //console.log('xyk', [x, y, keys]);
+        //logField(field, x, y);
 
         let min = Infinity;
         for (let i = 0; i < field.length; i += 1) {

34116972965c1d69033adc8691386e2b6754c06d
Matus Horvath <matus.horvath@gmail.com>
2019-12-27 05:12:45 +0100

    2019 18 a slow

--- 2019/18/src/a.ts
+++ 2019/18/src/a.ts
@@ -33,29 +33,61 @@ const logField = (field: string[][], x: number, y: number) => {
     console.log('----------');
 };
 
-const main = async () => {
-//    const input = await fs.readFile('input', 'utf8');
+const isSubset = (k1: string, k2: string) => {
+    let i1 = 0;
+    let i2 = 0;
 
-//     const input = 
-// `#########
-// #b.A.@.a#
-// #########
-// `;
-//     const input =
-// `########################
-// #f.D.E.e.C.b.A.@.a.B.c.#
-// ######################.#
-// #d.....................#
-// ########################
-// `;
-// const input =
-// `########################
-// #...............b.C.D.f#
-// #.######################
-// #.....@.a.B.c.d.A.e.F.g#
-// ########################
-// `;
-const input =
+    while (i1 < k1.length) {
+        while (i2 < k2.length && k2[i2] < k1[i1]) i2 += 1;
+        if (k2[i2] !== k1[i1]) return false;
+        i1 += 1;
+    }
+    return true;
+};
+
+const mergeVals = (vals: { [k: string]: number }[][], i: number, j: number, keys: string, newVal: number) => {
+    const keysToRemove = Object.entries(vals[i]?.[j] ?? {})
+        .filter(([k, v]) => v >= newVal && isSubset(k, keys))
+        .map(([k, v]) => k);
+
+    //console.log('1', vals);
+    for (const key of keysToRemove) {
+        delete vals[i][j][key];
+    }
+    //console.log('2', vals);
+
+    const betterKeys = Object.entries(vals[i]?.[j] ?? {})
+        .filter(([k, v]) => v <= newVal && isSubset(keys, k))
+        .map(([k, v]) => k);
+
+    if (betterKeys.length === 0) {
+        vset(vals, i, j, keys, newVal);
+    }
+};
+
+const main = async () => {
+    const input0 = await fs.readFile('input', 'utf8');
+
+    const input1 = 
+`#########
+#b.A.@.a#
+#########
+`;
+    const input2 =
+`########################
+#f.D.E.e.C.b.A.@.a.B.c.#
+######################.#
+#d.....................#
+########################
+`;
+const input3 =
+`########################
+#...............b.C.D.f#
+#.######################
+#.....@.a.B.c.d.A.e.F.g#
+########################
+`;
+const input4 =
 `#################
 #i.G..c...e..H.p#
 ########.########
@@ -66,12 +98,13 @@ const input =
 #l.F..d...h..C.m#
 #################
 `;
+const input = input4;
 
     const field = input.trimRight().split(/\r?\n/).map(r => r.split(''));
 
     let [x, y] = field.reduce((tr, r, ri) => tr[1] >= 0 ? tr : [ri, r.indexOf('@')], [-1, -1]);
     aset(field, x, y, '.');
-    logField(field, x, y);
+    //logField(field, x, y);
 
     const mxk = field.reduce((tr, r) => [...tr, ...r.filter(c => c >= 'a' && c <= 'z')], []).sort().pop();
     const kl = mxk.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
@@ -82,24 +115,29 @@ const input =
     vset(vals, x, y, '', 0);
 
     let keys = '';
+    let maxLen = 0;
 
     while (keys.length < kl) {
         for (const [i, j] of [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]]) {
             const isKey = field[i][j] >= 'a' && field[i][j] <= 'z';
             const isDoor = field[i][j] >= 'A' && field[i][j] <= 'Z';
 
-            const newKeys = isKey ? [...new Set([field[i][j], ...keys.split('')])].sort().join('') : keys;
-
             if (field[i][j] === '.' || isKey || (isDoor && keys.indexOf(field[i][j].toLowerCase()) >= 0)) {
-                vset(vals, i, j, newKeys, Math.min(vals[i]?.[j]?.[newKeys] ?? Infinity, vals[x][y][keys] + 1));
+                const newKeys = isKey ? [...new Set([field[i][j], ...keys.split('')])].sort().join('') : keys;
+                mergeVals(vals, i, j, newKeys, vals[x][y][keys] + 1);
             }
         }
 
         vset(vist, x, y, keys, true);
 
-        //console.log(vist)
-        console.log('xyk', [x, y, keys]);
-        logField(field, x, y);
+        if (keys.length > maxLen) {
+            maxLen = keys.length;
+            console.log('mxl', maxLen);
+        }
+        // console.log(vals)
+        // console.log(vist)
+        //console.log('xyk', [x, y, keys]);
+        // logField(field, x, y);
 
         let min = Infinity;
         for (let i = 0; i < field.length; i += 1) {

c55ebd6267be33b5a31b444c715bce21dc959803
Matus Horvath <matus.horvath@gmail.com>
2020-12-01 10:43:50 +0100

    Day 01

---
+++ 2020/01/a.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := ioutil.ReadFile("input")
+	if err != nil {
+		log.Fatalf("%v\n", err)
+	}
+	data := strings.Split(strings.TrimSpace(string(f)), "\n")
+
+	nums := make(map[int]bool)
+
+	for _, item := range data {
+		num, err := strconv.Atoi(item)
+		if err != nil {
+			log.Fatalf("parse error: %v\n", err)
+		}
+		_, has := nums[num]
+		if (!has && num < 2020) {
+			nums[num] = true;
+		}
+	}
+
+	for i := range nums {
+		for j := range nums {
+			for k := range nums {
+				if (i + j + k == 2020) {
+					log.Print(i*j*k)
+				}
+			}
+		}
+	}
+}

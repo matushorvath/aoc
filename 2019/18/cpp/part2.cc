1f23376925e332cda4fd32efa427b63cdb9726a3
Matus Horvath <matus.horvath@gmail.com>
2020-12-28 18:12:32 +0100

    Day 2019/18 part 2 improved

--- 2019/18/cpp/part2.cc
+++ 2019/18/cpp/part2.cc
@@ -7,6 +7,8 @@
 #include <tuple>
 #include <cstdint>
 #include <set>
+#include <map>
+#include <bitset>
 
 std::vector<std::vector<char>> load(const std::string& filename) {
 	std::vector<std::vector<char>> out;
@@ -45,10 +47,17 @@ std::tuple<std::vector<std::tuple<int, int>>, int> init(std::vector<std::vector<
 	return { robots, keyCount };
 }
 
-void print(const std::vector<std::vector<char>>& field) {
-	std::ostreambuf_iterator<char> itr(std::cout);
-	for (auto&& line : field) {
-		std::copy(line.begin(), line.end(), itr);
+const char* HILT = "\033[1;31m";
+const char* DFLT = "\033[0m";
+
+void print(const std::vector<std::vector<char>>& field, const std::vector<std::tuple<int, int>>& robots = {}) {
+	std::set<std::tuple<int, int>> rset{ robots.begin(), robots.end() };
+
+	for (size_t i = 0; i < field.size(); ++i) {
+		for (size_t j = 0; j < field[i].size(); ++j) {
+			bool hl = rset.count({ i, j }) > 0;
+			std::cout << (hl ? HILT : "") << field[i][j] << (hl ? DFLT : "");
+		}
 		std::cout << std::endl;
 	}
 }
@@ -61,12 +70,39 @@ bool haveDoorKey(uint32_t keys, char door) {
 	return (keys & (1 << (door - 'A'))) != 0;
 }
 
+bool isSubsetOf(uint32_t a, const std::set<uint32_t>& ks) {
+	for (auto b : ks) {
+		if (((a ^ b) & a) == 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
+void mergeKey(uint32_t a, std::set<uint32_t>& ks) {
+	for (auto itr = ks.begin(); itr != ks.end();) {
+		auto& b = *itr;
+		if (((a ^ b) & a) == 0) {
+			// Superset of a is already in ks
+			return;
+		}
+		if (((a ^ b) & b) == 0) {
+			// The a keyset is a superset of b, remove b
+			ks.erase(itr++);
+			std::cout << 'E';
+		} else {
+			++itr;
+		}
+	}
+	ks.insert(a);
+}
+
 int main() {
-	//auto field = load("sample3_8");
+	auto field = load("sample3_8");
 	//auto field = load("sample4_24");
 	//auto field = load("sample5_32");
 	//auto field = load("sample6_72");
-	auto field = load("../input");
+	//auto field = load("../input");
 	// print(field);
 
 	auto [robots, keyCount] = init(field);
@@ -79,8 +115,8 @@ int main() {
 	// std::cout << std::get<0>(positions[3]) << ',' << std::get<1>(positions[3]) << ',' << std::get<2>(positions[3]) << std::endl;
 	// std::cout << keyCount << ' ' << allKeys << std::endl;
 
-	std::set<std::tuple<std::vector<std::tuple<int, int>>, uint32_t>> border{ { robots, 0 } };
-	std::set<std::tuple<std::vector<std::tuple<int, int>>, uint32_t>> visited{ { robots, 0 } };
+	std::map<std::vector<std::tuple<int, int>>, std::set<uint32_t>> border{ { robots, { 0 } } };
+	std::map<std::vector<std::tuple<int, int>>, std::set<uint32_t>> visited{ { robots, { 0 } } };
 
 	int steps = 0;
 
@@ -90,47 +126,57 @@ int main() {
 		std::cout << "========" << std::endl;
 		std::cout << "Step: " << steps << std::endl;
 
-		std::set<std::tuple<std::vector<std::tuple<int, int>>, uint32_t>> newBorder;
+		std::map<std::vector<std::tuple<int, int>>, std::set<uint32_t>> newBorder;
 
-		for (auto&& [robots, k] : border) {
-			for (size_t ri = 0; ri < robots.size(); ++ri) {
-				auto&& [x, y] = robots[ri];
+		for (auto&& [robots, ks] : border) {
+			for (auto&& k : ks) {
+				for (size_t ri = 0; ri < robots.size(); ++ri) {
+					auto&& [x, y] = robots[ri];
 
-				std::initializer_list<std::tuple<int, int>> nbors = {
-					{ x - 1, y }, { x + 1, y }, { x, y - 1 }, { x, y + 1 }
-				};
+					std::initializer_list<std::tuple<int, int>> nbors = {
+						{ x - 1, y }, { x + 1, y }, { x, y - 1 }, { x, y + 1 }
+					};
 
-				for (auto&& [nx, ny] : nbors) {
-					bool isKey = field[nx][ny] >= 'a' && field[nx][ny] <= 'z';
-					bool isDoor = field[nx][ny] >= 'A' && field[nx][ny] <= 'Z';
+					for (auto&& [nx, ny] : nbors) {
+						bool isKey = field[nx][ny] >= 'a' && field[nx][ny] <= 'z';
+						bool isDoor = field[nx][ny] >= 'A' && field[nx][ny] <= 'Z';
 
-					if (field[nx][ny] == '.' || isKey || (isDoor && haveDoorKey(k, field[nx][ny]))) {
-						uint32_t nk = isKey ? addKey(k, field[nx][ny]) : k;
-						if (nk == allKeys) {
-							std::cout << "Result: " << steps << std::endl;
-							return 0;
-						}
+						if (field[nx][ny] == '.' || isKey || (isDoor && haveDoorKey(k, field[nx][ny]))) {
+							uint32_t nk = isKey ? addKey(k, field[nx][ny]) : k;
+							if (nk == allKeys) {
+								std::cout << "Result: " << steps << std::endl;
+								return 0;
+							}
 
-						auto newRobots = robots;
-						newRobots[ri] = { nx, ny };
+							auto newRobots = robots;
+							newRobots[ri] = { nx, ny };
 
-						if (visited.count({ newRobots, nk }) == 0) {
-							newBorder.emplace(newRobots, nk);
+							if (!isSubsetOf(nk, visited[newRobots])) {
+								mergeKey(nk, newBorder[newRobots]);
+							}
 						}
 					}
 				}
 			}
 		}
 
+		std::cout << '-';
 		swap(border, newBorder);
-		visited.insert(border.begin(), border.end());
+		for (auto&& [robots, ks] : border) {
+			for (auto&& k : ks) {
+				mergeKey(k, visited[robots]);
+			}
+		}
 		std::cout << "V: " << visited.size() << "; B: " << border.size() << std::endl;
 
-		// for (auto&& [robots, k] : border) {
-		// 	for (auto&& [x, y] : robots) {
-		// 		std::cout << '[' << x << "," << y << "] ";
+		// for (auto&& [robots, ks] : border) {
+		// 	for (auto&& k : ks) {
+		// 		for (auto&& [x, y] : robots) {
+		// 			std::cout << '[' << x << "," << y << "] ";
+		// 		}
+		// 		std::cout << "K " << std::bitset<26>(k) << " (" << k << ")" << std::endl;
+		// 		print(field, robots);
 		// 	}
-		// 	std::cout << "K " << k << std::endl;
 		// }
 		// for (auto&& [x, y, k, r] : visited) {
 		// 	std::cout << "V " << x << "," << y << "," << k << "," << r << std::endl;

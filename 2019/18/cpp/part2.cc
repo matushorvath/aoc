2dd3499038061c6c0523e9d64ff0c08bba85bd24
Matus Horvath <matus.horvath@gmail.com>
2020-12-28 16:25:02 +0100

    Day 2019/18 part 2 works, slow

--- 2019/18/cpp/part2.cc
+++ 2019/18/cpp/part2.cc
@@ -23,17 +23,17 @@ std::vector<std::vector<char>> load(const std::string& filename) {
 	return out;
 }
 
-std::tuple<std::vector<std::tuple<int, int, uint32_t, int>>, int> init(std::vector<std::vector<char>>& field) {
-	std::vector<std::tuple<int, int, uint32_t, int>> positions;
+std::tuple<std::vector<std::tuple<int, int>>, int> init(std::vector<std::vector<char>>& field) {
+	std::vector<std::tuple<int, int>> robots;
 	int keyCount = 0;
 
 	for (size_t i = 0; i < field.size(); ++i) {
 		for (size_t j = 0; j < field[i].size(); ++j) {
 			if (field[i][j] == '@') {
-				positions.emplace_back(i - 1, j - 1, 0, 0);
-				positions.emplace_back(i - 1, j + 1, 0, 1);
-				positions.emplace_back(i + 1, j - 1, 0, 2);
-				positions.emplace_back(i + 1, j + 1, 0, 3);
+				robots.emplace_back(i - 1, j - 1);
+				robots.emplace_back(i - 1, j + 1);
+				robots.emplace_back(i + 1, j - 1);
+				robots.emplace_back(i + 1, j + 1);
 
 				field[i][j] = field[i - 1][j] = field[i + 1][j] = field[i][j - 1] = field[i][j + 1] = '#';
 				field[i - 1][j - 1] = field[i - 1][j + 1] = field[i + 1][j - 1] = field[i + 1][j + 1] = '.';
@@ -42,7 +42,7 @@ std::tuple<std::vector<std::tuple<int, int, uint32_t, int>>, int> init(std::vect
 			}
 		}
 	}
-	return { positions, keyCount };
+	return { robots, keyCount };
 }
 
 void print(const std::vector<std::vector<char>>& field) {
@@ -62,14 +62,14 @@ bool haveDoorKey(uint32_t keys, char door) {
 }
 
 int main() {
-	auto field = load("sample3_8");
+	//auto field = load("sample3_8");
 	//auto field = load("sample4_24");
-	// auto field = load("sample5_32");
-	// auto field = load("sample6_72");
-	// auto field = load("../input");
+	//auto field = load("sample5_32");
+	//auto field = load("sample6_72");
+	auto field = load("../input");
 	// print(field);
 
-	auto [positions, keyCount] = init(field);
+	auto [robots, keyCount] = init(field);
 	uint32_t allKeys = (1 << keyCount) - 1;
 
 	print(field);
@@ -79,8 +79,8 @@ int main() {
 	// std::cout << std::get<0>(positions[3]) << ',' << std::get<1>(positions[3]) << ',' << std::get<2>(positions[3]) << std::endl;
 	// std::cout << keyCount << ' ' << allKeys << std::endl;
 
-	std::set<std::tuple<int, int, uint32_t, int>> border{ positions.begin(), positions.end() };
-	std::set<std::tuple<int, int, uint32_t, int>> visited{ positions.begin(), positions.end() };
+	std::set<std::tuple<std::vector<std::tuple<int, int>>, uint32_t>> border{ { robots, 0 } };
+	std::set<std::tuple<std::vector<std::tuple<int, int>>, uint32_t>> visited{ { robots, 0 } };
 
 	int steps = 0;
 
@@ -90,48 +90,48 @@ int main() {
 		std::cout << "========" << std::endl;
 		std::cout << "Step: " << steps << std::endl;
 
-		std::set<std::tuple<int, int, uint32_t, int>> newBorder;
-		for (auto&& [x, y, k, r] : border) {
-			std::initializer_list<std::tuple<int, int>> nbors = {
-				{ x - 1, y }, { x + 1, y }, { x, y - 1 }, { x, y + 1 }
-			};
+		std::set<std::tuple<std::vector<std::tuple<int, int>>, uint32_t>> newBorder;
 
-			for (auto&& [nx, ny] : nbors) {
-				bool isKey = field[nx][ny] >= 'a' && field[nx][ny] <= 'z';
-				bool isDoor = field[nx][ny] >= 'A' && field[nx][ny] <= 'Z';
+		for (auto&& [robots, k] : border) {
+			for (size_t ri = 0; ri < robots.size(); ++ri) {
+				auto&& [x, y] = robots[ri];
 
-				if (field[nx][ny] == '.' || isKey || (isDoor && haveDoorKey(k, field[nx][ny]))) {
-					uint32_t nk = isKey ? addKey(k, field[nx][ny]) : k;
-					if (nk == allKeys) {
-						std::cout << "Result: " << steps << std::endl;
-						return 0;
-					}
-					if (visited.count({ nx, ny, nk, r }) == 0) {
-						newBorder.emplace(nx, ny, nk, r);
-					}
-				}
-				if (isDoor && !haveDoorKey(k, field[nx][ny])) {
-					newBorder.emplace(x, y, k, r);
-				}
-				if (isKey) {
-					for (auto&& [ax, ay, ak, ar] : border) {
-						uint32_t newAk = addKey(ak, field[nx][ny]);
-						if (ar != r && visited.count({ ax, ay, newAk, ar }) == 0) {
-							newBorder.emplace(ax, ay, newAk, ar);
+				std::initializer_list<std::tuple<int, int>> nbors = {
+					{ x - 1, y }, { x + 1, y }, { x, y - 1 }, { x, y + 1 }
+				};
+
+				for (auto&& [nx, ny] : nbors) {
+					bool isKey = field[nx][ny] >= 'a' && field[nx][ny] <= 'z';
+					bool isDoor = field[nx][ny] >= 'A' && field[nx][ny] <= 'Z';
+
+					if (field[nx][ny] == '.' || isKey || (isDoor && haveDoorKey(k, field[nx][ny]))) {
+						uint32_t nk = isKey ? addKey(k, field[nx][ny]) : k;
+						if (nk == allKeys) {
+							std::cout << "Result: " << steps << std::endl;
+							return 0;
+						}
+
+						auto newRobots = robots;
+						newRobots[ri] = { nx, ny };
+
+						if (visited.count({ newRobots, nk }) == 0) {
+							newBorder.emplace(newRobots, nk);
 						}
 					}
 				}
 			}
 		}
 
-		// TODO remove from border where same position with more keys exists
-
 		swap(border, newBorder);
 		visited.insert(border.begin(), border.end());
+		std::cout << "V: " << visited.size() << "; B: " << border.size() << std::endl;
 
-		for (auto&& [x, y, k, r] : border) {
-			std::cout << "B " << x << "," << y << "," << k << "," << r << std::endl;
-		}
+		// for (auto&& [robots, k] : border) {
+		// 	for (auto&& [x, y] : robots) {
+		// 		std::cout << '[' << x << "," << y << "] ";
+		// 	}
+		// 	std::cout << "K " << k << std::endl;
+		// }
 		// for (auto&& [x, y, k, r] : visited) {
 		// 	std::cout << "V " << x << "," << y << "," << k << "," << r << std::endl;
 		// }

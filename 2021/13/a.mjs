213dbf9f950b920b0d038c919d09e9d627610131
Matus Horvath <matus.horvath@gmail.com>
2021-12-13 06:33:44 +0100

    Day 2021/13

---
+++ 2021/13/a.mjs
@@ -0,0 +1,59 @@
+import fs from 'fs/promises';
+
+const print = (p, Xs, Ys) => {
+    let cnt = 0;
+    const out = []
+    for (let y = 0; y < Ys; y++) {
+        for (let x = 0; x < Xs; x++) {
+            out.push(p[x]?.[y] ? '#' : '.');
+            if (p[x]?.[y]) cnt++;
+        }
+        out.push('\n');
+    }
+    console.log(out.join(''), cnt);
+    return cnt;
+};
+
+const main = async () => {
+    const input = await fs.readFile('input', 'utf8');
+    const data = input.trimEnd().split(/\r?\n/);
+
+    const mid = data.findIndex(l => l === '');
+    const pts = data.slice(0, mid).map(l => l.split(',').map(Number));
+    const fds = data.slice(mid + 1).map(l => {
+        const m = l.match(/fold along ([xy])=([0-9]+)/);
+        return [m[1], Number(m[2])];
+    });
+
+    console.log(pts);
+    console.log(fds);
+
+    let p = [];
+
+    let Xs = 0;
+    let Ys = 0;
+
+    for (const [x, y] of pts) {
+        if (p[x] === undefined) p[x] = [];
+        p[x][y] = 1;
+        if (x >= Xs) Xs = x + 1;
+        if (y >= Ys) Ys = y + 1;
+    }
+
+    print(p, Xs, Ys);
+
+    for (let x = 0; x < Xs / 2; x++) {
+        for (let y = 0; y < Ys; y++) {
+            if (p[Xs - x - 1]?.[y]) {
+                if (p[x] === undefined) p[x] = [];
+                p[x][y] = p[Xs - x - 1][y];
+            }
+        }
+    }
+    Xs = Math.floor(Xs / 2);
+    p = p.slice(0, Xs);
+
+    print(p, Xs, Ys);
+};
+
+await main();

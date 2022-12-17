c1ed50944a6fd31b611d107d77add3280fcddb6f
Matus Horvath <matus.horvath@gmail.com>
2022-12-17 16:57:16 +0100

    AoC 2022 Day 11 typescript

---
+++ 2022/11/b.ts
@@ -0,0 +1,62 @@
+'use strict';
+
+import fs from 'fs/promises';
+
+const main = async () => {
+    //const input = await fs.readFile('example', 'utf8');
+    const input = await fs.readFile('input', 'utf8');
+    let data = input.trimEnd().split(/\r?\n\r?\n/);
+
+    const monkeys: any[] = [];
+
+    for (const monkey of data) {
+        const m = monkey.match(/Monkey (\d):\n  Starting items: (.+)\n  Operation: (.+)\n  Test: divisible by (\d+)\n    If true: throw to monkey (\d)\n    If false: throw to monkey (\d)/);
+        const [, id, items, op, test, tid, fid] = m ?? [];
+
+        const op_renamed = op.replaceAll('new', 'neues');
+
+        monkeys[Number(id)] = {
+            id: Number(id),
+            items: items.split(', ').map(Number),
+            op: (old: Number) => { let neues; eval(op_renamed); return neues; },
+            test: Number(test),
+            tid: Number(tid),
+            fid: Number(fid),
+            inspects: 0
+        };
+    }
+
+    const mod = [...new Set(monkeys.map(m => m.test))].reduce((p, c) => p * c, 1);
+
+    for (let round = 0; round < 10000; round++) {
+        for (const monkey of monkeys) {
+            //console.log('Monkey ', monkey.id);
+
+            let item;
+            while (item = monkey.items.shift()) {
+                //console.log('  Inspect ', item);
+
+                item = monkey.op(item);
+                //console.log('    Increase to ', item);
+
+                item = item % mod;
+                //console.log('    Reduce to ', item);
+
+                const res = item % monkey.test === 0;
+                //console.log('    Test is ', res);
+
+                const target = res ? monkey.tid : monkey.fid;
+                monkeys[target].items.push(item);
+                //console.log('    Throw to ', target);
+
+                monkey.inspects++;
+            }
+        }
+    }
+
+    const sorted = monkeys.sort((a, b) => b.inspects - a.inspects);
+    console.log(sorted[0].inspects * sorted[1].inspects);
+    //console.log(sorted.map(m => m.inspects));
+};
+
+main().catch(e => console.error(e));

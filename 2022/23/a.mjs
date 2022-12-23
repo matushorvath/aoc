8faf8a7c81e13f2c12d2b348cb760d4c578197d8
Matus Horvath <matus.horvath@sap.com>
2022-12-23 07:13:39 +0100

    AoC 2022 Day 23

---
+++ 2022/23/a.mjs
@@ -0,0 +1,111 @@
+'use strict';
+
+import fs from 'fs/promises';
+
+const print = (data) => {
+    const [mnx, mxx] = Object.keys(data).map(Number).reduce((p, c) => [c < p[0] ? c : p[0], c > p[1] ? c : p[1]], [Infinity, -Infinity]);
+    const [mny, mxy] = Object.values(data).reduce((pr, cr) => {
+        const [mn, mx] = Object.keys(cr).map(Number).reduce((p, c) => [c < p[0] ? c : p[0], c > p[1] ? c : p[1]], [Infinity, -Infinity]);
+        return [Math.min(pr[0], mn), Math.max(pr[1], mx)];
+    }, [Infinity, -Infinity]);
+
+    for (let x = mnx; x <= mxx; x++) {
+        let out = '';
+        for (let y = mny; y <= mxy; y++) {
+            out += data[x]?.[y] === '#' ? '#' : '.';
+        }
+        console.log(out);
+    }
+};
+
+const score = (data) => {
+    const [mnx, mxx] = Object.keys(data).map(Number).reduce((p, c) => [c < p[0] ? c : p[0], c > p[1] ? c : p[1]], [Infinity, -Infinity]);
+    const [mny, mxy] = Object.values(data).reduce((pr, cr) => {
+        const [mn, mx] = Object.keys(cr).map(Number).reduce((p, c) => [c < p[0] ? c : p[0], c > p[1] ? c : p[1]], [Infinity, -Infinity]);
+        return [Math.min(pr[0], mn), Math.max(pr[1], mx)];
+    }, [Infinity, -Infinity]);
+
+    let cnt = 0;
+
+    for (let x = mnx; x <= mxx; x++) {
+        for (let y = mny; y <= mxy; y++) {
+            if (!data[x]?.[y]) cnt++;
+        }
+    }
+
+    return cnt;
+};
+
+const main = async () => {
+    //const input = await fs.readFile('example-1', 'utf8');
+    //const input = await fs.readFile('example-2', 'utf8');
+    const input = await fs.readFile('input', 'utf8');
+    const data = Object.fromEntries(input.trimEnd().split(/\r?\n/).map((l, x) => [
+        x, Object.fromEntries(l.split('').map((c, y) => [y, c]).filter(([y, c]) => c !== '.'))]));
+
+    print(data);
+
+    const dirs = [
+        { check: [[-1, -1], [-1, 0], [-1, 1]], move: [-1, 0] },
+        { check: [[ 1, -1], [ 1, 0], [ 1, 1]], move: [ 1, 0] },
+        { check: [[-1, -1], [0, -1], [1, -1]], move: [0, -1] },
+        { check: [[-1,  1], [0,  1], [1,  1]], move: [0,  1] }
+    ];
+
+    for (let round = 0; round < 10; round++) {
+        const props = {};
+
+        const [mnx, mxx] = Object.keys(data).map(Number).reduce((p, c) => [c < p[0] ? c : p[0], c > p[1] ? c : p[1]], [Infinity, -Infinity]);
+        for (let x = mnx; x <= mxx; x++) {
+            const [mny, mxy] = Object.keys(data[x]).map(Number).reduce((p, c) => [c < p[0] ? c : p[0], c > p[1] ? c : p[1]], [Infinity, -Infinity]);
+            for (let y = mny; y <= mxy; y++) {
+                if (data[x][y] === '#' && (
+                        data[x-1]?.[y] || data[x+1]?.[y] ||
+                        data[x-1]?.[y-1] || data[x]?.[y-1] || data[x+1]?.[y-1] ||
+                        data[x-1]?.[y+1] || data[x]?.[y+1] || data[x+1]?.[y+1])) {
+
+                    let didx = round % 4;
+                    do {
+                        const dir = dirs[didx];
+
+                        if (!data[x+dir.check[0][0]]?.[y+dir.check[0][1]] &&
+                            !data[x+dir.check[1][0]]?.[y+dir.check[1][1]] &&
+                            !data[x+dir.check[2][0]]?.[y+dir.check[2][1]]) {
+
+                            if (!props[x+dir.move[0]]) props[x+dir.move[0]] = {};
+                            if (!props[x+dir.move[0]][y+dir.move[1]]) props[x+dir.move[0]][y+dir.move[1]] = [];
+                            props[x+dir.move[0]][y+dir.move[1]].push([x, y, didx]);
+
+                            break;
+                        }
+
+                        didx = (didx + 1) % 4;
+                    } while (didx !== round % 4)
+                }
+            }
+        }
+
+        for (const px in props) {
+            for (const py in props[px]) {
+                if (props[px][py].length === 1) {
+                    delete data[props[px][py][0][0]][props[px][py][0][1]];
+                }
+            }
+        }
+        for (const px in props) {
+            for (const py in props[px]) {
+                if (props[px][py].length === 1) {
+                    if (!data[px]) data[px] = {};
+                    data[px][py] = '#';
+                }
+            }
+        }
+
+        console.log(JSON.stringify(props));
+        print(data);
+    }
+
+    console.log(score(data));
+};
+
+await main();

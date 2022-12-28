ab65851d6c03a747edc2479632521c7e6a045bc1
Matus Horvath <matus.horvath@sap.com>
2022-12-28 03:22:50 +0100

    AoC 2022 Day 24 part 1 example works

---
+++ 2022/24/a.mjs
@@ -0,0 +1,72 @@
+'use strict';
+
+import fs from 'fs/promises';
+
+const main = async () => {
+    //const input = await fs.readFile('example', 'utf8');
+    const input = await fs.readFile('input', 'utf8');
+    const data = input.trimEnd().split(/\r?\n/).map(r => r.split(''));
+
+    const B = { x: 0, y: 1 };
+    const E = { x: data.length - 1, y: data[0].length - 2 };
+
+    const bl = data.flatMap((r, x) => r.reduce((p, d, y) => {
+        if (d === '>' || d === '<' || d === 'v' || d === '^') {
+            return [...p, { d, x, y }];
+        } else {
+            return p;
+        }
+    }, []));
+
+    const stack = [{ ...B, bl, t: 0}];
+    const seen = new Set();
+
+    const mkkey = s => `${s.x} ${s.y} ${s.bl.map(b => `[${b.d} ${b.x} ${b.y}]`).join(' ')}`;
+
+    const normx = x => (x - 1 + data.length - 2) % (data.length - 2) + 1;
+    const normy = y => (y - 1 + data[0].length - 2) % (data[0].length - 2) + 1;
+
+    const advance = bl => bl.map(({ d, x, y }) => {
+        switch (d) {
+            case '>': return { d, x, y: normy(y + 1) };
+            case '<': return { d, x, y: normy(y - 1) };
+            case 'v': return { d, x: normx(x + 1), y };
+            case '^': return { d, x: normx(x - 1), y };
+        }
+    });
+
+    let patht = Infinity;
+
+    let s;
+    while (s = stack.pop()) {
+        if (s.bl.some(b => b.x === s.x && b.y === s.y)) {
+            continue;
+        }
+
+        const key = mkkey(s);
+        if (seen[key] < s.t || patht < s.t) continue;
+        seen[key] = s.t;
+        if (s.x === E.x && s.y === E.y) patht = s.t;
+
+        const newbl = advance(s.bl);
+
+        stack.push({ x: s.x, y: s.y, bl: newbl, t: s.t + 1 });
+
+        if (s.x > 0 && data[s.x - 1][s.y] !== '#') {
+            stack.push({ x: s.x - 1, y: s.y, bl: newbl, t: s.t + 1 });
+        }
+        if (s.y > 0 && data[s.x][s.y - 1] !== '#') {
+            stack.push({ x: s.x, y: s.y - 1, bl: newbl, t: s.t + 1 });
+        }
+        if (s.x < data.length - 1 && data[s.x + 1][s.y] !== '#') {
+            stack.push({ x: s.x + 1, y: s.y, bl: newbl, t: s.t + 1 });
+        }
+        if (s.y < data[0].length - 1 && data[s.x][s.y + 1] !== '#') {
+            stack.push({ x: s.x, y: s.y + 1, bl: newbl, t: s.t + 1 });
+        }
+    }
+
+    console.log('>', patht);
+};
+
+await main();

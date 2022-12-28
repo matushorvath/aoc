8e98967fd869a5c953ea66d28f48bd4af3af1eb2
Matus Horvath <matus.horvath@sap.com>
2022-12-28 03:59:13 +0100

    AoC 2022 Day 24 part 1

--- 2022/24/a.mjs
+++ 2022/24/a.mjs
@@ -1,6 +1,7 @@
 'use strict';
 
 import fs from 'fs/promises';
+import lcm from 'compute-lcm';
 
 // napad:
 // blizardy maju periodu pocet riadkov * pocet stlpcov
@@ -9,26 +10,18 @@ import fs from 'fs/promises';
 
 // NODE_OPTIONS=--max_old_space_size=16384 node a.mjs
 
-const main = async () => {
-    //const input = await fs.readFile('example', 'utf8');
-    const input = await fs.readFile('input', 'utf8');
-    const data = input.trimEnd().split(/\r?\n/).map(r => r.split(''));
+// 554 best found for part 1
 
-    const B = { x: 0, y: 1 };
-    const E = { x: data.length - 1, y: data[0].length - 2 };
+const makebls = (data) => {
+    const bls = [];
 
-    const bl = data.flatMap((r, x) => r.reduce((p, d, y) => {
+    bls[0] = data.flatMap((r, x) => r.reduce((p, d, y) => {
         if (d === '>' || d === '<' || d === 'v' || d === '^') {
             return [...p, { d, x, y }];
         } else {
             return p;
         }
     }, []));
-
-    const stack = [{ ...B, bl, t: 0}];
-    const seen = new Set();
-
-    const mkkey = s => `${s.x} ${s.y} ${s.bl.map(b => `[${b.d} ${b.x} ${b.y}]`).join(' ')}`;
 
     const normx = x => (x - 1 + data.length - 2) % (data.length - 2) + 1;
     const normy = y => (y - 1 + data[0].length - 2) % (data[0].length - 2) + 1;
@@ -42,17 +35,41 @@ const main = async () => {
         }
     });
 
+    const period = lcm(data.length - 2, data[0].length - 2);
+    console.log('period', period);
+
+    for (let i = 1; i < period; i++) {
+        bls[i] = advance(bls[i - 1]);
+    }
+
+    return { period, bls };
+};
+
+const main = async () => {
+    //const input = await fs.readFile('example', 'utf8');
+    const input = await fs.readFile('input', 'utf8');
+    const data = input.trimEnd().split(/\r?\n/).map(r => r.split(''));
+
+    const { period, bls } = makebls(data);
+
+    const B = { x: 0, y: 1 };
+    const E = { x: data.length - 1, y: data[0].length - 2 };
+
+    const stack = [{ ...B, t: 0}];
+    const seen = new Set();
+
     let patht = Infinity;
 
     let s;
     while (s = stack.pop()) {
         if (patht <= s.t) continue;
 
-        const key = mkkey(s);
+        const key = `${s.x} ${s.y} ${s.t % period}`;
         if (seen[key] <= s.t) continue;
         seen[key] = s.t;
 
-        if (s.bl.some(b => b.x === s.x && b.y === s.y)) {
+        const thisbl = bls[s.t % period];
+        if (thisbl.some(b => b.x === s.x && b.y === s.y)) {
             continue;
         }
 
@@ -61,21 +78,19 @@ const main = async () => {
             console.log(patht, 'stack', stack.length, 'seen', seen.length);
         }
 
-        const newbl = advance(s.bl);
-
-        stack.push({ x: s.x, y: s.y, bl: newbl, t: s.t + 1 });
+        stack.push({ x: s.x, y: s.y, t: s.t + 1 });
 
         if (s.x > 0 && data[s.x - 1][s.y] !== '#') {
-            stack.push({ x: s.x - 1, y: s.y, bl: newbl, t: s.t + 1 });
+            stack.push({ x: s.x - 1, y: s.y, t: s.t + 1 });
         }
         if (s.y > 0 && data[s.x][s.y - 1] !== '#') {
-            stack.push({ x: s.x, y: s.y - 1, bl: newbl, t: s.t + 1 });
+            stack.push({ x: s.x, y: s.y - 1, t: s.t + 1 });
         }
         if (s.x < data.length - 1 && data[s.x + 1][s.y] !== '#') {
-            stack.push({ x: s.x + 1, y: s.y, bl: newbl, t: s.t + 1 });
+            stack.push({ x: s.x + 1, y: s.y, t: s.t + 1 });
         }
         if (s.y < data[0].length - 1 && data[s.x][s.y + 1] !== '#') {
-            stack.push({ x: s.x, y: s.y + 1, bl: newbl, t: s.t + 1 });
+            stack.push({ x: s.x, y: s.y + 1, t: s.t + 1 });
         }
     }
 

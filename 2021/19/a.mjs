ce071cb5a7f97d4f7dc246011111c44148891c10
Matus Horvath <matus.horvath@sap.com>
2023-01-02 05:20:20 +0100

    Map bids between scanners

--- 2021/19/a.mjs
+++ 2021/19/a.mjs
@@ -1,6 +1,7 @@
 'use strict';
 
 import fs from 'fs/promises';
+import { connect } from 'http2';
 import stringify from "json-stringify-pretty-compact";
 
 const main = async () => {
@@ -19,7 +20,7 @@ const main = async () => {
             })
         }));
 
-    //console.log(stringify(data, { maxLength: 160 }));
+    //console.log(stringify(data, { maxLength: 240 }));
 
     // Calc relative coordinates between beacons within one scanner
     for (const scanner of data) {
@@ -32,7 +33,7 @@ const main = async () => {
         }
     }
 
-    //console.log(stringify(data.map(s => s.rel), { maxLength: 160 }));
+    //console.log(stringify(data.map(s => s.rel), { maxLength: 240 }));
 
     // Calc relative coordinates between beacons under each of the 24 possible scanner orientations; sort them in a repeatable way
     const compareRelCoords = (a, b) => {
@@ -86,13 +87,13 @@ const main = async () => {
         while (i1 < distances1.length && i2 < distances2.length) {
             const cmp = compareRelCoords(distances1[i1], distances2[i2]);
             if (cmp < 0) {
-                extra1.push(distances2[i2]);
+                extra1.push(distances1[i1]);
                 i1++;
             } else if (cmp > 0) {
                 extra2.push(distances2[i2]);
                 i2++;
             } else {
-                common.push(distances1[i1]);
+                common.push({ bid3: distances2[i2].bid1, bid4: distances2[i2].bid2, ...distances1[i1] });
                 i1++; i2++;
             }
         }
@@ -135,8 +136,23 @@ const main = async () => {
                     const [, mul0, mul1, mul2, rot0, rot1, rot2] = m.map(Number);
                     //console.log(mul0, mul1, mul2, rot0, rot1, rot2);
 
-                    const commonBids = [...new Set(common.flatMap(({ bid1, bid2 }) => [bid1, bid2]))].sort((a, b) => a - b);
+                    const commonBids2 = [...new Set(common.flatMap(({ bid3, bid4 }) => [bid3, bid4]))].sort((a, b) => a - b);
                     //console.log(scanner1.sid, scanner2.sid, commonBids);
+
+                    const bidMap = {}; // map scanner 2 bid to scanner 1 bid
+                    // bid1-4 are very confusing in this cycle, careful
+                    for (const bid2 of commonBids2) {
+                        const connections = common.filter(({ bid3, bid4 }) => bid2 === bid3 || bid2 === bid4);
+                        if (connections.every(({ bid1, bid2 }) => bid1 === connections[0].bid1 || bid2 === connections[0].bid1)) {
+                            // bid2 corresponds to connections[0].bid1
+                            bidMap[bid2] = connections[0].bid1;
+                        } else if (connections.every(({ bid1, bid2 }) => bid1 === connections[0].bid2 || bid2 === connections[0].bid2)) {
+                            // bid2 corresponds to connections[0].bid2
+                            bidMap[bid2] = connections[0].bid2;
+                        } else throw new Error('wtf');
+                    }
+
+                    console.log(scanner1.sid, scanner2.sid, rot, stringify(bidMap, { maxLength: 240 }));
 
                     // TODO determine scanner 2 position rel to data[0]
                     // = scanner 1 position rel to data[0] + beacon position rel to scanner 1 - beacon position rel to scanner 2 (I think converted using orientation)
@@ -146,27 +162,27 @@ const main = async () => {
                     // TODO Then map each beacon position rel to data[0]
                     // = (I think) Convert beacon position using orientation (done below) + scanner 2 position rel to data[0]
 
-                    const beacons = [];
-                    for (const bid of commonBids) {
-                        const tmp = [];
-                        tmp[rot0] = scanner2.beacons[bid].loc[0] * mul0 || 0;
-                        tmp[rot1] = scanner2.beacons[bid].loc[1] * mul1 || 0;
-                        tmp[rot2] = scanner2.beacons[bid].loc[2] * mul2 || 0;
-                        beacons.push(tmp);
-                    }
-                    candidate_beacons.push({
-                        orientation: { mul0, mul1, mul2, rot: [rot0, rot1, rot2] },
-                        beacons
-                    })
+                    // const beacons = [];
+                    // for (const bid2 of commonBids2) {
+                    //     const tmp = [];
+                    //     tmp[rot0] = scanner2.beacons[bid2].loc[0] * mul0 || 0;
+                    //     tmp[rot1] = scanner2.beacons[bid2].loc[1] * mul1 || 0;
+                    //     tmp[rot2] = scanner2.beacons[bid2].loc[2] * mul2 || 0;
+                    //     beacons.push(tmp);
+                    // }
+                    // candidate_beacons.push({
+                    //     orientation: { mul0, mul1, mul2, rot: [rot0, rot1, rot2] },
+                    //     beacons
+                    // })
                 }
             }
 
-            console.log(stringify(candidate_beacons, { maxLength: 160 }));
+            console.log(stringify(candidate_beacons, { maxLength: 240 }));
             break;
         }
     }
 
-    //console.log(stringify(data.map(s => s.rotated), { maxLength: 160 }));
+    //console.log(stringify(data.map(s => s.rotated), { maxLength: 240 }));
 };
 
 await main();

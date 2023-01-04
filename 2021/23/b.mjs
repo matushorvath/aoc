b2520a1ab66fab83eb17972766cdf4624571d4cb
Matus Horvath <matus.horvath@sap.com>
2023-01-04 02:48:31 +0100

    AoC 2021 Day 23 new approach started

--- 2021/23/b.mjs
+++ 2021/23/b.mjs
@@ -2,24 +2,7 @@
 
 import fs from 'fs/promises';
 
-const getenergy = type => 10 ** type;
-const getdesty = type => 3 + type * 2;
-
-//const print = data => console.log(data.map(row => row.join('')).join('\n'));
-const print = (data, pos) => {
-    let out = data.map(row => row.map(ch => ch >= 'A' && ch <= 'D' ? '.' : ch));
-    for (const { t, x, y } of pos) {
-        out[x][y] = String.fromCharCode('A'.charCodeAt(0) + t);
-    }
-    console.log(out.map(row => row.join('')).join('\n'));
-};
-
-//const clone = data => [...data.map(row => [...row])];
-const clone = pos => [...pos.map(am => ({ ...am }))];
-
-const mkkey = (pos, moved) => `${moved} ` + pos.map(({ t, x, y }) => `${t},${x},${y}`).join(' ');
-
-const occ = (pos, ox, oy) => pos.find(({ x, y }) => ox === x && oy === y);
+const mkkey = pos => `${pos.lend} ${pos.rend} ${pos.mids} ${pos.rooms[0]} ${pos.rooms[1]} ${pos.rooms[2]} ${pos.rooms[3]}`;
 
 const main = async () => {
     //const input = await fs.readFile('inputa.done', 'utf8');
@@ -28,128 +11,200 @@ const main = async () => {
     //const input = await fs.readFile('inputa', 'utf8');
     //const input = await fs.readFile('inputb', 'utf8');
 
-    const data = input.trimEnd().split(/\r?\n/).map(l => l.split(''));
+    const amphipods = input.trimEnd().split(/\r?\n/).slice(2, -1)
+        .map(row => [row[3], row[5], row[7], row[9]].map(ch => ch.charCodeAt(0) - 'A'.charCodeAt(0)));
 
-    const initpos = [];
-    for (let x = 0; x < data.length; x++) {
-        for (let y = 0; y < data[x].length; y++) {
-            if (data[x][y] >= 'A' && data[x][y] <= 'D') {
-                const t = data[x][y].charCodeAt(0) - 'A'.charCodeAt(0);
-                initpos.push({ t, x, y });
-            }
+    const CAP = amphipods.length;
+
+    const print = (pos) => {
+        const lends = pos.lend.map(a => String.fromCharCode('A'.charCodeAt(0) + a)).join('').padEnd(2, '.');
+        const rends = pos.rend.map(a => String.fromCharCode('A'.charCodeAt(0) + a)).join('').padStart(2, '.');
+        const midss = pos.mids.map(a => a >= 0 ? String.fromCharCode('A'.charCodeAt(0) + a) : '.').join('').padEnd(3, '.');
+        const roomss = pos.rooms.map(room => room.map(a => String.fromCharCode('A'.charCodeAt(0) + a)));
+
+        console.log('#############');
+        console.log(`#${lends}.${midss[0]}.${midss[1]}.${midss[2]}.${rends}#`)
+        for (let i = 0; i < CAP; i++) {
+            console.log([
+                `${i === 0 ? '##' : '  '}#`,
+                `${roomss[0][CAP - i - 1] ?? '.'}#`,
+                `${roomss[1][CAP - i - 1] ?? '.'}#`,
+                `${roomss[2][CAP - i - 1] ?? '.'}#`,
+                `${roomss[3][CAP - i - 1] ?? '.'}#`,
+                `${i === 0 ? '##' : '  '}`
+            ].join(''));
         }
-    }
-
-    //console.log(initpos);
-
-    const MAXX = data.length - 2;
+        console.log('  #########  ', '\n');
+    };
+ 
+    const data = {
+        lend: [], rend: [], mids: [-1, -1, -1],
+        rooms: amphipods[0].map((_, c) => amphipods.map((_, r) => amphipods[CAP - r - 1][c]))
+    };
 
     const todo = [];
-    todo.push({ pos: initpos, moved: -1, energy: 0 });
+    todo.push({ pos: data, energy: 0 });
 
     const seen = {};
     let minenergy = Infinity;
 
-    let iter = 0;
-
     let state;
     while (state = todo.pop()) {
-        const { pos, moved, energy } = state;
+        print(state.pos);
+
+        const { pos, energy } = state;
+        const { lend, rend, mids, rooms } = pos;
 
         if (energy > minenergy) continue;
 
-        const key = mkkey(pos, moved);
+        const key = mkkey(pos);
         if (seen[key] && seen[key] < energy) continue;
         seen[key] = energy;
 
-        if (pos.every(({ t, x, y }) => x >= 2 && y === getdesty(t))) {
+        if (lend.length === 0 && rend.length === 0 && mids.every(a => a === -1)
+                && rooms.every((room, rtype) => room.every(atype => atype === rtype))) {
             minenergy = energy;
             continue;
         }
 
-        iter++;
-        if (iter % 100000 === 0) {
-            print(data, pos); console.log(moved, energy, '\n', 'iter', iter, 'todo', todo.length, 'mine', minenergy, '\n');
-        }
+        // Move topmost ams in rooms
+        {
+            // Room 0
+            const rtype = 0;
+            const room = rooms[rtype];
+            const atype = room[room.length - 1];
 
-        for (let i = 0; i < pos.length; i++) {
-            const { t, x, y } = pos[i];
-            const nenergy = energy + getenergy(t);
-            const desty = getdesty(t);
+            // Move if in incorrect room, or if foreigners are below
+            const move = atype !== rtype || !room.every(oatype => oatype === atype);
 
-            if (x >= 2) {
-                // A in room:
-                let canMoveUp = true
-                if (y === desty) {
-                    canMoveUp = pos.every(({ ot, ox, oy }) => oy !== desty || ox < x || ot === t);
-                }
-
-                // - within this room
-                if (x < MAXX && !occ(pos, x + 1, y)) {
-                    const npos = clone(pos);
-                    npos[i].x = x + 1;
-                    todo.push({ pos: npos, moved: -1, energy: nenergy });
-                }
-                if (canMoveUp && x > 2 && !occ(pos, x - 1, y)) {
-                    const npos = clone(pos);
-                    npos[i].x = x - 1;
-                    todo.push({ pos: npos, moved: -1, energy: nenergy });
-                }
-
-                // - to hallway but not in front of any exit
-                // TODO optimize: stay in its own room unless there's foreigners below
-                // TODO optimize: if we move to left or right end, move as far as possible only (don't move to y = 2 if y = 1 is free)
-                if (canMoveUp && x === 2 && !occ(pos, x - 1, y - 1)) {
-                    const npos = clone(pos);
-                    npos[i].x = x - 1; npos[i].y = y - 1;
-                    todo.push({ pos: npos, moved: i, energy: nenergy });
-                }
-                if (canMoveUp && x === 2 && !occ(pos, x - 1, y + 1)) {
-                    const npos = clone(pos);
-                    npos[i].x = x - 1; npos[i].y = y + 1;
-                    todo.push({ pos: npos, moved: i, energy: nenergy });
-                }
-            } else if (x === 1) {
-                // A in hallway:
-                // - to target room unless it has any foreigners
-                if ((y === desty - 1 || y === desty + 1) && !occ(pos, 1, desty) && !occ(pos, 2, desty)
-                        && pos.every(({ ot, oy }) => oy !== desty || ot === t)) {
-                    const npos = clone(pos);
-                    npos[i].x = 2; npos[i].y = desty;
-                    todo.push({ pos: npos, moved: -1, energy: nenergy });
-                }
-                // TODO move as far down as possible, stay there
-
-                if (moved === i) {
-                    // A in hallway, last that moved:
-                    // - to hallway but not in front of any exit
-                    if ((y === 2 || y === 4 || y === 6 || y === 8) && !occ(pos, 1, y + 1) && !occ(pos, 1, y + 2)) {
-                        // Move by 2, so we don't stop in front of exit
-                        const npos = clone(pos);
-                        npos[i].x = 1; npos[i].y = y + 2;
-                        todo.push({ pos: npos, moved: i, energy: nenergy });
-                    } else if (y < 11 && !occ(pos, 1, y + 1)) {
-                        const npos = clone(pos);
-                        npos[i].x = 1; npos[i].y = y + 1;
-                        todo.push({ pos: npos, moved: i, energy: nenergy });
-                    }
-
-                    if ((y === 4 || y === 6 || y === 8 || y === 10) && !occ(pos, 1, y - 1) && !occ(pos, 1, y - 2)) {
-                        // Move by 2, so we don't stop in front of exit
-                        const npos = clone(pos);
-                        npos[i].x = 1; npos[i].y = y - 2;
-                        todo.push({ pos: npos, moved: i, energy: nenergy });
-                    } else if (y > 1 && !occ(pos, 1, y - 1)) {
-                        const npos = clone(pos);
-                        npos[i].x = 1; npos[i].y = y - 1;
-                        todo.push({ pos: npos, moved: i, energy: nenergy });
-                    }
-                }
+            if (move) {
+                if (lend.length < 2) todo.push({
+                    pos: {
+                        lend: [...lend, atype],
+                        rend,
+                        mids,
+                        rooms: [[...room.slice(0, -1)], rooms[1], rooms[2], rooms[3]]
+                    },
+                    energy: energy + ((CAP - room.length) + 1 + (2 - lend.length)) * (10 ** atype)
+                });
+                if (mids[0] < 0) todo.push({
+                    pos: {
+                        lend,
+                        rend,
+                        mids: [atype, mids[1], mids[2]],
+                        rooms: [[...room.slice(0, -1)], rooms[1], rooms[2], rooms[3]]
+                    },
+                    energy: energy + ((CAP - room.length) + 1 + 1) * (10 ** atype)
+                });
+                if (mids[0] < 0 && mids[1] < 0) todo.push({
+                    pos: {
+                        lend,
+                        rend,
+                        mids: [mids[0], atype, mids[2]],
+                        rooms: [[...room.slice(0, -1)], rooms[1], rooms[2], rooms[3]]
+                    },
+                    energy: energy + ((CAP - room.length) + 1 + 1) * (10 ** atype)
+                });
+                if (mids[0] < 0 && mids[1] < 0 && mids[2] < 0) todo.push({
+                    pos: {
+                        lend,
+                        rend,
+                        mids: [mids[0], mids[1], atype],
+                        rooms: [[...room.slice(0, -1)], rooms[1], rooms[2], rooms[3]]
+                    },
+                    energy: energy + ((CAP - room.length) + 1 + 1) * (10 ** atype)
+                });
+                if (mids[0] < 0 && mids[1] < 0 && mids[2] < 0 && rend.length < 2) todo.push({
+                    pos: {
+                        lend,
+                        rend:  [...rend, atype],
+                        mids,
+                        rooms: [[...room.slice(0, -1)], rooms[1], rooms[2], rooms[3]]
+                    },
+                    energy: energy + ((CAP - room.length) + 1 + (2 - lend.length)) * (10 ** atype)
+                });
             }
         }
     }
 
     console.log(minenergy);
+
+
+        //     for (let i = 0; i < pos.length; i++) {
+    //         const { t, x, y } = pos[i];
+    //         const nenergy = energy + 10 ** t;
+    //         const desty = getdesty(t);
+
+    //         if (x >= 2) {
+    //             // A in room:
+    //             let canMoveUp = true
+    //             if (y === desty) {
+    //                 canMoveUp = pos.every(({ ot, ox, oy }) => oy !== desty || ox < x || ot === t);
+    //             }
+
+    //             // - within this room
+    //             if (x < MAXX && !occ(pos, x + 1, y)) {
+    //                 const npos = clone(pos);
+    //                 npos[i].x = x + 1;
+    //                 todo.push({ pos: npos, moved: -1, energy: nenergy });
+    //             }
+    //             if (canMoveUp && x > 2 && !occ(pos, x - 1, y)) {
+    //                 const npos = clone(pos);
+    //                 npos[i].x = x - 1;
+    //                 todo.push({ pos: npos, moved: -1, energy: nenergy });
+    //             }
+
+    //             // - to hallway but not in front of any exit
+    //             // TODO optimize: stay in its own room unless there's foreigners below
+    //             // TODO optimize: if we move to left or right end, move as far as possible only (don't move to y = 2 if y = 1 is free)
+    //             if (canMoveUp && x === 2 && !occ(pos, x - 1, y - 1)) {
+    //                 const npos = clone(pos);
+    //                 npos[i].x = x - 1; npos[i].y = y - 1;
+    //                 todo.push({ pos: npos, moved: i, energy: nenergy });
+    //             }
+    //             if (canMoveUp && x === 2 && !occ(pos, x - 1, y + 1)) {
+    //                 const npos = clone(pos);
+    //                 npos[i].x = x - 1; npos[i].y = y + 1;
+    //                 todo.push({ pos: npos, moved: i, energy: nenergy });
+    //             }
+    //         } else if (x === 1) {
+    //             // A in hallway:
+    //             // - to target room unless it has any foreigners
+    //             if ((y === desty - 1 || y === desty + 1) && !occ(pos, 1, desty) && !occ(pos, 2, desty)
+    //                     && pos.every(({ ot, oy }) => oy !== desty || ot === t)) {
+    //                 const npos = clone(pos);
+    //                 npos[i].x = 2; npos[i].y = desty;
+    //                 todo.push({ pos: npos, moved: -1, energy: nenergy });
+    //             }
+    //             // TODO move as far down as possible, stay there
+
+    //             if (moved === i) {
+    //                 // A in hallway, last that moved:
+    //                 // - to hallway but not in front of any exit
+    //                 if ((y === 2 || y === 4 || y === 6 || y === 8) && !occ(pos, 1, y + 1) && !occ(pos, 1, y + 2)) {
+    //                     // Move by 2, so we don't stop in front of exit
+    //                     const npos = clone(pos);
+    //                     npos[i].x = 1; npos[i].y = y + 2;
+    //                     todo.push({ pos: npos, moved: i, energy: nenergy });
+    //                 } else if (y < 11 && !occ(pos, 1, y + 1)) {
+    //                     const npos = clone(pos);
+    //                     npos[i].x = 1; npos[i].y = y + 1;
+    //                     todo.push({ pos: npos, moved: i, energy: nenergy });
+    //                 }
+
+    //                 if ((y === 4 || y === 6 || y === 8 || y === 10) && !occ(pos, 1, y - 1) && !occ(pos, 1, y - 2)) {
+    //                     // Move by 2, so we don't stop in front of exit
+    //                     const npos = clone(pos);
+    //                     npos[i].x = 1; npos[i].y = y - 2;
+    //                     todo.push({ pos: npos, moved: i, energy: nenergy });
+    //                 } else if (y > 1 && !occ(pos, 1, y - 1)) {
+    //                     const npos = clone(pos);
+    //                     npos[i].x = 1; npos[i].y = y - 1;
+    //                     todo.push({ pos: npos, moved: i, energy: nenergy });
+    //                 }
+    //             }
+    //         }
+    //     }
 };
 
 await main();

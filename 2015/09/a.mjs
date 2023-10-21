93f8cf433b010c925f60a3c36f22e7e7b176ff6a
Matus Horvath <matus.horvath@gmail.com>
2023-10-21 14:40:56 +0200

    AoC 2015 Day 9 part 1

--- 2015/09/a.mjs
+++ 2015/09/a.mjs
@@ -1,7 +1,8 @@
 import fs from 'fs/promises';
+import os from 'os';
 
-const input = await fs.readFile('example', 'utf8');
-//const input = await fs.readFile('input', 'utf8');
+//const input = await fs.readFile('example', 'utf8');
+const input = await fs.readFile('input', 'utf8');
 
 const data = input.trimEnd().split(/\r?\n/).map(r => {
     const m = r.match(/^(\w+) to (\w+) = (\d+)$/);
@@ -14,52 +15,36 @@ const i2n = [...new Set([...data.map(d => d.src), ...data.map(d => d.tgt)])].sor
 const n2i = Object.fromEntries(i2n.map((n, i) => [n, i]));
 
 // console.log(i2n);
-console.log(n2i);
+// console.log(n2i);
 
-const visited = [];
-const dist = [];
 const lens = [];
-
 for (let i = 0; i < i2n.length; i++) {
-    visited[i] = [];
-    dist[i] = [];
     lens[i] = [];
-
     for (let j = 0; j < i2n.length; j++) {
-        visited[i][j] = false;
-        dist[i][j] = i === j ? 0 : Infinity;
-        // console.log(i, j);
         lens[i][j] = i === j ? 0 : data.find(({ src, tgt }) => (n2i[src] === i && n2i[tgt] === j) || (n2i[src] === j && n2i[tgt] === i)).len;
     }
 }
 
-const mindist = (dist, visited) => {
-    let min = Infinity;
-    let mi, mj;
+console.log(lens);
 
-    for (let i = 0; i < dist.length; i++) {
-        for (let j = 0; j < dist.length; j++) {
-            if (!visited[i][j] && dist[i][j] <= min) {
-                mi = i; mj = j; min = dist[i][j];
+let paths = i2n.map((n, i) => ({ done: new Set([i]), last: i, length: 0 }));
+
+for (let i = 1; i < i2n.length; i++) {
+    const new_paths = [];
+    for (const path of paths) {
+        for (let j = 0; j < i2n.length; j++) {
+            if (!path.done.has(j)) {
+                new_paths.push({
+                    done: new Set([...path.done, j]),
+                    last: j,
+                    length: path.length + lens[path.last][j]
+                });
             }
         }
     }
 
-    return [mi, mj];
-};
-
-while (true) {
-    let [csrc, ctgt] = mindist(dist, visited);
-    if (csrc === undefined && ctgt === undefined) break;
-
-    for (let j = 0; j < dist.length; j++) {
-        if (j !== ctgt) {
-            dist[csrc][j] = Math.min(dist[csrc][j], dist[csrc][ctgt] + lens[ctgt][j]);
-        }
-    }
-
-    visited[csrc][ctgt] = true;
-    console.log(visited, dist);
+    paths = new_paths;
+    console.log(paths);
 }
 
-console.log(visited, dist);
+console.log('result', paths.reduce((p, c) => Math.min(p, c.length), Infinity));

5e95829c0f02990ff81cebd524dc130c5dccc95d
Matus Horvath <matus.horvath@gmail.com>
2023-11-12 21:07:16 +0100

    AoC 2015 Day 17 part 1

--- 2015/17/a.mjs
+++ 2015/17/a.mjs
@@ -6,10 +6,10 @@ Array.prototype.toSpliced = function (...args) {
     return tmp;
 };
 
-const input = await fs.readFile('example', 'utf8');
-const tvol = 25;
-//const input = await fs.readFile('input', 'utf8');
-//const tvol = 150;
+// const input = await fs.readFile('example', 'utf8');
+// const tvol = 25;
+const input = await fs.readFile('input', 'utf8');
+const tvol = 150;
 
 const data = input.trimEnd().split(/\r?\n/).map(Number);
 
@@ -17,17 +17,27 @@ const data = input.trimEnd().split(/\r?\n/).map(Number);
 
 const q = [{ vol: 0, filled: [], empty: [...data.keys()] }];
 const results = new Set();
+const memo = new Set();
+
+let iter = 0;
 
 while (q.length > 0) {
+    if (++iter % 100000 === 0) console.log(iter, results.size);
+
     const { vol, filled, empty } = q.pop();
     for (const [idx, adding] of Object.entries(empty)) {
         const nvol = vol + data[adding];
         const nfilled = [...filled, adding];
+        const nfkey = nfilled.sort((a, b) => a - b).join(',');
+
         if (nvol === tvol) {
-            console.log(nfilled.map(i => data[i]));
-            results.add(JSON.stringify(nfilled.sort((a, b) => a - b)));
+            //console.log(nfilled.map(i => data[i]));
+            results.add(nfkey);
         } else if (nvol < tvol) {
-            q.push({ vol: nvol, filled: nfilled, empty: empty.toSpliced(idx, 1) });
+            if (!memo.has(nfkey)) {
+                memo.add(nfkey);
+                q.push({ vol: nvol, filled: nfilled, empty: empty.toSpliced(idx, 1) });
+            }
         }
     }
 }
